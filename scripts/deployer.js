f5adca4d4d6741b01f17552b9f59b0fe35697ce1
Axe <ngsmo@iscte-iul.pt>
2021-05-16 13:59:17 +0100

    working deployer and verifier

--- scripts/deployer.js
+++ scripts/deployer.js
@@ -1,10 +1,18 @@
+const { execSync } = require("child_process");
 const merkleFactory = artifacts.require("MerkleDistributor");
 
-module.exports = async function (callback, argv) {
+module.exports = async function (callback) {
     try {
+        const network = process.argv[process.argv.length - 1];
         const drop = await merkleFactory.new();
-        console.log("drop deployed at", drop.address);
+        console.log(`${network} - MerkleDistributor@${drop.address}`);
+        const cmd = `truffle run verify MerkleDistributor@${drop.address} --network ${network}`;
+        execSync(cmd, {
+			stdio: 'inherit',
+		} );
+        callback();
     } catch(err) {
         console.error(err);
+        callback(err);
     }
 }
